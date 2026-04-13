9fcea964d0b5b1a83cc2be4d703b05dae62cd08b
piotr-skolorzynski <p.skolorzynski@gmail.com>
2026-04-13 18:42:28 +0200

    Add mocked books data Return books on get request

--- Project1/books.py
+++ Project1/books.py
@@ -2,7 +2,28 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+BOOKS = [
+    {"title": "Diuna", "author": "Frank Herbert", "category": "Sci-Fi"},
+    {
+        "title": "Wiedźmin: Ostatnie życzenie",
+        "author": "Andrzej Sapkowski",
+        "category": "Fantasy",
+    },
+    {"title": "Hobbit", "author": "J.R.R. Tolkien", "category": "Fantasy"},
+    {
+        "title": "Morderstwo w Orient Expressie",
+        "author": "Agatha Christie",
+        "category": "Kryminał",
+    },
+    {"title": "Rok 1984", "author": "George Orwell", "category": "Dystopia"},
+    {
+        "title": "Cień wiatru",
+        "author": "Carlos Ruiz Zafón",
+        "category": "Literatura piękna",
+    },
+]
 
-@app.get("/api-endpoint")
+
+@app.get("/books")
 async def get_books():  # w fast api async jest dodawany automatycznie, ale można go dodać ręcznie
-    return {"message": "Hello, Skolo"}
+    return BOOKS
