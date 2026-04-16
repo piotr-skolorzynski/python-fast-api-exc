c930663659680797bb7f2b9e9834024da19cef1c
piotr-skolorzynski <p.skolorzynski@gmail.com>
2026-04-16 19:25:58 +0200

    Create post entpoint to create a new todo in database

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,10 +1,12 @@
 from typing import Annotated
+from xmlrpc.client import boolean
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends, HTTPException, Path
 from models import Todos
 import models
 from database import SessionLocal, engine
 from starlette import status
+from pydantic import BaseModel, Field
 
 # główny plik gdzie będzie się działa zarządzanie całym fastApi projektu
 
@@ -28,6 +30,14 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+# id jako primary key zostanie dodany automatycznie w bazce
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: boolean
+
+
 @app.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
@@ -38,5 +48,11 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
     if todo_model is not None:
         return todo_model
-
     raise HTTPException(status_code=404, detail="Todo not found.")
+
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.model_dump())
+    db.add(todo_model)  # dodaj do tabeli
+    db.commit()  # zapisz
