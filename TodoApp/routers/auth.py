fe9aadf43f530ac5bbaecb813af3d0ed135ea39a
piotr-skolorzynski <p.skolorzynski@gmail.com>
2026-04-17 18:33:35 +0200

    Encode JWT

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta, timezone
 from database import SessionLocal
 from fastapi import APIRouter, Depends
 from starlette import status
@@ -6,12 +7,20 @@ from models import Users
 from passlib.context import CryptContext
 from typing import Annotated
 from sqlalchemy.orm import Session
+from dotenv import load_dotenv
+import os
 from fastapi.security import (
     OAuth2PasswordRequestForm,
 )  # zapewnia w swagerze możliwość użycia bardziej bezpiecznego formularza wymagającego podania hasła
+from jose import jwt
 
 # żeby endpointy z tego pliku były w instancji fastApi z main.py musimy odziedziczyć routing
 router = APIRouter()
+
+# konfigurowanie JWT
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
 
 # hashowanie
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -24,6 +33,12 @@ class CreateUserRequest(BaseModel):
     last_name: str
     password: str
     role: str
+
+
+# model określający odpowiedź z tokenem oraz dodatkowymi informacjami
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 
 def get_db():
@@ -46,7 +61,15 @@ def authenticate_user(username: str, password: str, db):
         password, user.hashed_password
     ):  # sprawdamy czy hasło zgadza się z hashowanym hasłem
         return False
-    return True
+    return user
+
+
+# generate access tokens
+def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+    encode = {"sub": username, "id": user_id}
+    expires = datetime.now(timezone.utc) + expires_delta
+    encode.update({"exp": expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
 # teraz w endpointach dekoratory piszemy @router i on przekaże endpoint do instancji fastApi z main.py
@@ -68,15 +91,15 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
     db.commit()
 
 
-@router.post("/token")
+@router.post("/token", response_model=Token)
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency
 ):  # teraz swagger będzie podawał formularz
-    is_user_authenticated = authenticate_user(
-        form_data.username, form_data.password, db
-    )
+    user = authenticate_user(form_data.username, form_data.password, db)
 
-    if not is_user_authenticated:
+    if not user:
         return "Failed Authentication"
 
-    return "Successful Authentication"
+    token = create_access_token(user.username, user.id, timedelta(minutes=20))
+
+    return {"access_token": token, "token_type": "bearer"}
