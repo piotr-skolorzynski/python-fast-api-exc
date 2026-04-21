540c749687a5412f53d3e6946de3cc67a2146fa2
piotr-skolorzynski <p.skolorzynski@gmail.com>
2026-04-21 11:47:07 +0200

    Switch to postgreSQL db

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -7,10 +7,13 @@ from dotenv import load_dotenv
 load_dotenv()
 SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
-# tworzy naszą bazę danych
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# tworzy naszą bazę danych sqlite3
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
+
+# tworzy naszą bazę danych postgreSQL
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
