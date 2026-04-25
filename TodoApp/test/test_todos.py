80b175adcd9edb76ce730a58418ce4c178cc19b1
piotr-skolorzynski <p.skolorzynski@gmail.com>
2026-04-25 13:33:06 +0200

    Todos testing Add test to check create todo

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -1,3 +1,4 @@
+import json
 import os
 import pytest
 from dotenv import load_dotenv
@@ -118,3 +119,22 @@ def test_read_one_authenticated_not_found(client, setup_db, test_todo):
     response = client.get("/todo/999")
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert response.json() == {"detail": "Todo not found."}
+
+
+def test_create_todo(client, setup_db, test_todo):
+    request_data = {
+        "title": "New Todo!",
+        "description": "New todo description",
+        "priority": 5,
+        "complete": False,
+    }
+
+    response = client.post("/todo/", json=request_data)
+    assert response.status_code == status.HTTP_201_CREATED
+
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id == 2).first()
+    assert model.title == request_data.get("title")
+    assert model.description == request_data.get("description")
+    assert model.priority == request_data.get("priority")
+    assert model.complete == request_data.get("complete")
