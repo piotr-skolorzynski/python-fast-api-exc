761cb2c86f5e5c01921a4c975db05483aca65dff
piotr-skolorzynski <p.skolorzynski@gmail.com>
2026-04-28 11:34:56 +0200

    Setup jinja prepare css and js to start working on a client side

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import os
 from .routers import todos, auth, admin, users
 from .models import Base
 from .database import engine
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 # główny plik gdzie będzie się działa zarządzanie całym fastApi projektu
 
@@ -13,6 +16,20 @@ app = FastAPI()
 Base.metadata.create_all(bind=engine)
 # utworzy bazke przy odpaleniu uvicorn na tym pliku uvicorn main:app --reload
 # (jeśli przeszedłeś na relative path to uvicorn TodoApp.main:app --reload), jeśli bazka jeszcze nie istnieje
+
+# wskazanie folderu z templatami jinja
+# templates = Jinja2Templates(directory="templates") - to nie zadziałało, musiałem złapać ścieżkę do pliku poniżej
+base_dir = os.path.dirname(os.path.realpath(__file__))
+templates = Jinja2Templates(directory=os.path.join(base_dir, "templates"))
+# dodajemy pliki statyczne z css i js
+app.mount(
+    "/static", StaticFiles(directory=os.path.join(base_dir, "static")), name="static"
+)
+
+
+@app.get("/")
+async def templates_test(request: Request):
+    return templates.TemplateResponse(request, "home.html", {"request": request})
 
 
 # health check test
