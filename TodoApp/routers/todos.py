86351ba8ad71b8d56a6ae34d8e5941227c4d8167
piotr-skolorzynski <p.skolorzynski@gmail.com>
2026-05-01 20:41:33 +0200

    Creat edit todo page

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -70,7 +70,7 @@ async def render_todo_page(request: Request, db: db_dependency):
 
 
 @router.get("/add-todo-page")
-async def render_todo_page(request: Request):
+async def render_add_todo_page(request: Request):
     try:
         user = await get_current_user(request.cookies.get("access_token"))
 
@@ -79,6 +79,24 @@ async def render_todo_page(request: Request):
         return templates.TemplateResponse(
             request, "add-todo.html", {"request": request, "user": user}
         )
+    except:
+        return redirect_to_login()
+
+
+@router.get("/edit-todo-page/{todo_id}")
+async def render_edit_todo_page(request: Request, todo_id: int, db: db_dependency):
+    try:
+        user = await get_current_user(request.cookies.get("access_token"))
+
+        if user is None:
+            return redirect_to_login()
+
+        todo = (db.query(Todos).filter(Todos.id == todo_id)).first()
+
+        return templates.TemplateResponse(
+            request, "edit-todo.html", {"request": request, "todo": todo, "user": user}
+        )
+
     except:
         return redirect_to_login()
 
